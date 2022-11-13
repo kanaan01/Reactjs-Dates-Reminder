c417f8a1fed520646482baf0072a682140059c0c
Mohammad kanaan <mkanaan0502@gmail.com>
2022-11-13 20:40:24 +0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { Container } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { person } from "./data";
+import DatesCount from "./components/DatesCount";
+import DatesList from "./components/DatesList";
+import DatesAction from "./components/DatesAction";
+
+function App() {
+  const [personData, setPersonData] = useState(person);
+  const onDelete = () => {
+    setPersonData([]);
+  };
+  const onViewData = () => {
+    setPersonData(person);
+  };
+  useEffect(() => {
+    setPersonData([]);
+  }, []);
+
+  return (
+    <div className="font color-body">
+      <Container fluid className="py-5">
+        <DatesCount person={person} />
+        <DatesList person={personData} />
+        <DatesAction deleteData={onDelete} onViewData={onViewData} />
+      </Container>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,9 @@
+export const person = [
+  { id: "0", name: "Mohammad", date: "5 PM", img: "p1.jpg" },
+  { id: "1", name: "Ali", date: "4 PM", img: "p1.jpg" },
+  { id: "2", name: "Ahamd", date: "3 PM", img: "p1.jpg" },
+  { id: "3", name: "Baker", date: "2 PM", img: "p1.jpg" },
+  { id: "4", name: "Hamza", date: "1 PM", img: "p1.jpg" },
+  { id: "5", name: "Hasan", date: "1 PM", img: "p1.jpg" },
+  { id: "6", name: "Gaith", date: "1 PM", img: "p1.jpg" },
+];
